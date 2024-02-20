8e058e6dfa83f415c703e949c9fe2033ae71609c
Shivam Sharma <meshivam81@gmail.com>
2024-02-20 10:05:01 +0530

    fix layout for large device

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <div className="bg-blue-800 py-10">
-      <div className="container sm:p-1 mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center">
         <span className="text-3xl text-white font-bold tracking-tight">
             AtHolidays.com
         </span>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     const { isLoggedIn } = useAppContext()
     return (
         <div className="bg-blue-800 py-6">
-            <div className="container sm:p-1 mx-auto flex justify-between">
+            <div className="container mx-auto flex justify-between">
                 <span className="text-3xl  text-white font-bold tracking-tight">
                     <Link to='/'>Holidays.com</Link>
                 </span>
