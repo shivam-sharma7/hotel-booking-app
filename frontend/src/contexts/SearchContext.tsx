b8d23536e99f3eb82e21fc82986279e5c4f5eed2
Shivam Sharma <meshivam81@gmail.com>
2024-05-19 11:59:33 +0530

    save search form to session storage

--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -23,12 +23,12 @@ type SearchContextProvider = {
 };
 
 export const SearchContextProvider = ({ children }: SearchContextProvider) => {
-  const [destination, setDestination] = useState("");
-  const [checkIn, setCheckIn] = useState<Date>(new Date());
-  const [checkOut, setCheckOut] = useState<Date>(new Date());
-  const [adultsCount, setAdultsCount] = useState<number>(1);
-  const [childCount, setChildCount] = useState<number>(0);
-    const [hotelId, setHotelId] = useState<string>("");
+  const [destination, setDestination] = useState(()=> sessionStorage.getItem("destination") || "" );
+  const [checkIn, setCheckIn] = useState<Date>(()=> new Date(sessionStorage.getItem("checkIn") || new Date().toISOString()));
+  const [checkOut, setCheckOut] = useState<Date>(()=> new Date(sessionStorage.getItem("checkOut") || new Date().toISOString()));
+  const [adultsCount, setAdultsCount] = useState<number>(()=> parseInt(sessionStorage.getItem("adultsCount") || '1'));
+  const [childCount, setChildCount] = useState<number>(()=> parseInt(sessionStorage.getItem("childCount") || '1'));
+    const [hotelId, setHotelId] = useState<string>(()=> sessionStorage.getItem("hotelId") || "");
 
   const saveSearchValues = (
     destination: string,
@@ -45,7 +45,17 @@ export const SearchContextProvider = ({ children }: SearchContextProvider) => {
     setChildCount(childCount);
     if (hotelId) {
       setHotelId(hotelId);
-    }   
+    } 
+    
+    sessionStorage.setItem("destination", destination)
+    sessionStorage.setItem("checkIn", checkIn.toISOString())
+    sessionStorage.setItem("checkOut", checkOut.toISOString())
+    sessionStorage.setItem("adultsCount", adultsCount.toString())
+    sessionStorage.setItem("childCount", childCount.toString())
+    if (hotelId) {
+      sessionStorage.setItem("hotelId", hotelId)
+    }
+    
   };
 
   return (
